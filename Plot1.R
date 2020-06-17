26e508b75c55f0b5cf1133dfe87cbc27ac42e4f9
vijayeshwari <vijayeshwarip@gmail.com>
2020-06-17 13:41:53 +0530

    Plot1

---
+++ Plot1.R
@@ -0,0 +1,6 @@
+total.emissions <- summarise(group_by(NEI, year), Emissions=sum(Emissions))
+clrs <- c("red", "green", "blue", "yellow")
+x1<-barplot(height=total.emissions$Emissions/1000, names.arg=total.emissions$year,
+            xlab="years", ylab=expression('total PM'[2.5]*' emission in kilotons'),ylim=c(0,8000),
+            main=expression('Total PM'[2.5]*' emissions at various years in kilotons'),col=clrs)
+text(x = x1, y = round(total.emissions$Emissions/1000,2), label = round(total.emissions$Emissions/1000,2), pos = 3, cex = 0.8, col = "black")
