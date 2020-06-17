44382d1630551ee69f13b265d6f91e7c888eeaf8
vijayeshwari <vijayeshwarip@gmail.com>
2020-06-17 13:43:01 +0530

    Plot2

---
+++ Plot2.R
@@ -0,0 +1,8 @@
+baltcitymary.emissions<-summarise(group_by(filter(NEI, fips == "24510"), year), Emissions=sum(Emissions))
+clrs <- c("red", "green", "blue", "yellow")
+x2<-barplot(height=baltcitymary.emissions$Emissions/1000, names.arg=baltcitymary.emissions$year,
+            xlab="years", ylab=expression('total PM'[2.5]*' emission in kilotons'),ylim=c(0,4),
+            main=expression('Total PM'[2.5]*' emissions in Baltimore City-MD in kilotons'),col=clrs)
+
+
+text(x = x2, y = round(baltcitymary.emissions$Emissions/1000,2), label = round(baltcitymary.emissions$Emissions/1000,2), pos = 3, cex = 0.8, col = "black")
